20d6b4e518323c175b5ed84763179959b7cb5821
kiran-nepali <cuziamnepali@hotmail.com>
2020-06-15 09:42:21 +0100

    Updates

---
+++ precache-manifest.5c505a4e087ea3ef344673be050967ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03cac966ffe02090da36882f1470949a",
+    "url": "/React-weatherApi/index.html"
+  },
+  {
+    "revision": "603cf90314d1a3298808",
+    "url": "/React-weatherApi/static/css/main.d55abcbb.chunk.css"
+  },
+  {
+    "revision": "273e7197b9b1e0842ee2",
+    "url": "/React-weatherApi/static/js/2.2f575353.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-weatherApi/static/js/2.2f575353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "603cf90314d1a3298808",
+    "url": "/React-weatherApi/static/js/main.26c4fe09.chunk.js"
+  },
+  {
+    "revision": "5dec4ce07a8c439d4fa0",
+    "url": "/React-weatherApi/static/js/runtime-main.c3f293d3.js"
+  }
+]);
